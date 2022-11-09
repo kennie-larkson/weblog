43df013e34cc1249fb104aad27427a467aa2d74d
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-11-09 10:34:20 +0100

    changed env

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 const express = require("express");
-const Sequelize = require("sequelize")
-const { validateForm, hashPassword } = require("./middleware/validation")
-require('dotenv').config()
+const Sequelize = require("sequelize");
+const { validateForm, hashPassword } = require("./middleware/validation");
+require("dotenv").config();
 
 const controller = require("./controllers");
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-const { DB_NAME, DB_USER, DB_PWD, DB_HOST, DB_DIALECT } = process.env
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const {
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_HOST,
+  DB_DIALECT,
+} = process.env;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PWD, {
-    host: DB_HOST,
-    dialect: DB_DIALECT
+const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
+  host: POSTGRES_HOST,
+  dialect: DB_DIALECT,
 });
-sequelize.authenticate().then(() => {
-    console.log("Success!");
-}).catch((err) => {
-    console.log(err);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection was successful!");
+  })
+  .catch((err) => {
+    console.log("Database connection error:", err);
+  });
 
 app.get("/", (req, res) => {
-    controller.home(req, res);
+  controller.home(req, res);
 });
 
 app.post("/api/v1/login", (req, res) => {
-    controller.login(req, res)
-
-})
+  controller.login(req, res);
+});
 
 app.post("/api/v1/signup", validateForm, hashPassword, (req, res) => {
-
-    controller.signup(req, res)
-})
+  controller.signup(req, res);
+});
 
 app.get("/api/v1/lessons", (req, res) => {
-    controller.getLessons(req, res)
-})
+  controller.getLessons(req, res);
+});
 
 app.listen(5000, () => console.log("Server running on port 5000"));
 
-module.exports = app
+module.exports = app;
--- middleware/validation.js
+++ middleware/validation.js
@@ -1,45 +1,43 @@
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 function validateForm(req, res, next) {
-    const {
-        childName,
-        email,
-        phoneNumber,
-        countryCode,
-        password,
-        confirmPassword,
-        grade,
-    } = req.body;
-    if (
-        !childName ||
-        !email ||
-        !phoneNumber ||
-        !countryCode ||
-        !password ||
-        !confirmPassword ||
-        !grade
-    ) {
-        console.log("Please enter missing fields");
-        res.status(404).json({ message: "Please enter missing fields" });
-        return;
-    } else if (password != confirmPassword) {
-        return res.status(404).json({ status: false, message: "Your passwords do not match" })
-    }
-    next()
+  const {
+    name,
+    email,
+    //phoneNumber,
+    //countryCode,
+    password,
+    confirmPassword,
+    //grade,
+  } = req.body;
+  if (
+    !name ||
+    !email ||
+    //!phoneNumber ||
+    //!countryCode ||
+    !password ||
+    !confirmPassword
+    //!grade
+  ) {
+    console.log("Please enter missing fields");
+    res.status(404).json({ message: "Please enter missing fields" });
+    return;
+  } else if (password != confirmPassword) {
+    return res
+      .status(404)
+      .json({ status: false, message: "Your passwords do not match" });
+  }
+  next();
 }
 
 async function hashPassword(req, res, next) {
-    const user = req.body
+  const user = req.body;
 
-    const hash = await bcrypt.hash(user.password, 8)
-    user.password = hash
-    req.user = user
-    next()
+  const hash = await bcrypt.hash(user.password, 8);
+  user.password = hash;
+  req.user = user;
+  next();
 }
 
-
-
-
-module.exports = { validateForm, hashPassword }
+module.exports = { validateForm, hashPassword };
