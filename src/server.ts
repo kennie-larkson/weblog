98d7d17c203c8df7f743865c5948c9fd572f0c3c
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2022-12-17 10:27:07 +0100

    added AirbnbController class to server IIFE in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import App from "./app";
 import PostsController from "./entity/posts/post.controller";
 //import TopicController from "./entity/topic/topic.entity";
 import UserController from "./entity/users/user.controller";
+import { AirbnbController } from "entity/airbnb-be/airbnb.controller";
 
 (async () => {
   const app = new App([
     new PostsController(),
     new UserController(),
     new AuthController(),
+    new AirbnbController(),
     //new TopicController(),
   ]);
 
